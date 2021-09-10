1a028cb19c00c30229d01b07ae12164e95e9a10a
ayshagazi <ayshagazi2477@gmail.com>
2021-09-10 21:36:15 +0600

    User Session Added

--- TechReviewPoint/Controllers/UsersController.cs
+++ TechReviewPoint/Controllers/UsersController.cs
@@ -39,29 +39,55 @@ namespace TechReviewPoint.Controllers
         {
             return View();
         }
+        public ActionResult UserDashboard()
+        {
+            return View();
+        }
 
         [HttpPost]
         public ActionResult Login(loginUser login)
         {
-            if (ModelState.IsValid)
-            {
-                var user = db.Users.Where(u => u.UserEmail.Equals(login.UserEmail)
-                 && u.UserPassword.Equals(login.UserPassword)).FirstOrDefault();
+            
+                if (ModelState.IsValid)
+                {
+                    var user = db.Users.Where(u => u.UserEmail.Equals(login.UserEmail)
+                     && u.UserPassword.Equals(login.UserPassword)).FirstOrDefault();
 
-                if (user != null)
-                {
-                    return Content("Login Successfull!!");
+                    if (user != null)
+                    {
+                    Session["UserSessionEmail"] = user.UserEmail;
+                    // return RedirectToAction("UserDashboard", new { email = user.UserEmail });
+                    return RedirectToAction("UserDashboard");
+
+                    }
+                    else
+                    {
+                       
+                        ViewBag.Loginfailed = "User not found or Password mismatch";
+                        return View();
+                      
+                    }
+
                 }
-                else
-                {
-                    return Content(" Login Failed!!");
-                }
-            }
+            
             return View();
+        }
+        public ActionResult UserLogout()
+        {
+            Session.Abandon();
+            return RedirectToAction("Login");
         }
 
 
+
+
+
+
         // GET: Users/Details/5
+
+
+
+
 
     }
 }
