4838b20eb3680d4c8e93a8911f6045837465293b
Purnendu Talukder <ptsourav21@gmail.com>
2021-09-10 14:37:21 +0600

    Admin Login and Session Added

---
+++ TechReviewPoint/Controllers/AdminsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Web.Mvc;
+using TechReviewPoint.Models;
+
+namespace TechReviewPoint.Controllers
+{
+    public class AdminsController : Controller
+    {
+        // GET: Admins
+        public ActionResult AdminLogin()
+        {
+            return View();
+        }
+        public ActionResult AdminDashboard()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Auth(Admin adminModel)
+        {
+            using(tech_review_pointEntities db = new tech_review_pointEntities())
+            {
+                var adminDetails = db.Admins.Where(x => x.AdminEmail == adminModel.AdminEmail && x.AdminPassword == adminModel.AdminPassword).FirstOrDefault();
+                if(adminDetails==null)
+                {
+                    adminModel.LoginErrorMessage = "wrong email or password";
+                    return View("AdminLogin", adminModel);
+                }
+                else
+                {
+                    Session["AdminSessionEmail"] = adminDetails.AdminEmail;
+                    return RedirectToAction("AdminDashboard");
+                }
+            }
+            return View();
+        }
+        public ActionResult AdminLogOut()
+        {
+            Session.Abandon();
+            return RedirectToAction("AdminLogin");
+        }
+    }
+}
--- TechReviewPoint/Models/Admin.cs
+++ TechReviewPoint/Models/Admin.cs
@@ -11,13 +11,21 @@ namespace TechReviewPoint.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Admin
     {
         public int AdminID { get; set; }
+        [DisplayName("Admin Email")]
+        [Required(ErrorMessage ="Field is required")]
         public string AdminEmail { get; set; }
         public string AdminName { get; set; }
         public string AdminImg { get; set; }
+        [DisplayName("Admin Password")]
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Field is required")]
         public string AdminPassword { get; set; }
+        public string LoginErrorMessage { get; set; }
     }
 }
