e7c4b137d75df60939b2bd50f0bb617e61411799
ayshagazi <ayshagazi2477@gmail.com>
2021-09-18 22:05:03 +0600

    Comment added

---
+++ TechReviewPoint/Controllers/CommentsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TechReviewPoint.Models;
+
+namespace TechReviewPoint.Controllers
+{
+    public class CommentsController : Controller
+    {
+        private tech_review_pointEntities db = new tech_review_pointEntities();
+
+        // GET: Comments
+        public ActionResult Index()
+        {
+            var comments = db.Comments.Include(c => c.Review).Include(c => c.User);
+            return View(comments.ToList());
+        }
+
+        // GET: Comments/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Comment comment = db.Comments.Find(id);
+            if (comment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comment);
+        }
+
+        // GET: Comments/Create
+        public ActionResult Create()
+        {
+            ViewBag.ReviewID = new SelectList(db.Reviews, "ReviewID", "ReviewPost");
+            ViewBag.UserID = new SelectList(db.Users, "UserID", "UserName");
+            return View();
+        }
+
+        // POST: Comments/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CommentID,UserID,ReviewID,Comment1,CommentDate")] Comment comment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Comments.Add(comment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ReviewID = new SelectList(db.Reviews, "ReviewID", "ReviewPost", comment.ReviewID);
+            ViewBag.UserID = new SelectList(db.Users, "UserID", "UserName", comment.UserID);
+            return View(comment);
+        }
+
+        // GET: Comments/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Comment comment = db.Comments.Find(id);
+            if (comment == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ReviewID = new SelectList(db.Reviews, "ReviewID", "ReviewPost", comment.ReviewID);
+            ViewBag.UserID = new SelectList(db.Users, "UserID", "UserName", comment.UserID);
+            return View(comment);
+        }
+
+        // POST: Comments/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CommentID,UserID,ReviewID,Comment1,CommentDate")] Comment comment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(comment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ReviewID = new SelectList(db.Reviews, "ReviewID", "ReviewPost", comment.ReviewID);
+            ViewBag.UserID = new SelectList(db.Users, "UserID", "UserName", comment.UserID);
+            return View(comment);
+        }
+
+        // GET: Comments/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Comment comment = db.Comments.Find(id);
+            if (comment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comment);
+        }
+
+        // POST: Comments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Comment comment = db.Comments.Find(id);
+            db.Comments.Remove(comment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
