345aeb44addf9259d012de7bd76f0d373d8a7a5f
Purnendu Talukder <ptsourav21@gmail.com>
2021-09-25 00:26:12 +0600

    Custom Layout (ProductDetailsLayout) Added

---
+++ TechReviewPoint/Controllers/IssueController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TechReviewPoint.Controllers
+{
+    public class IssueController : Controller
+    {
+        // GET: Issue
+        public ActionResult IssuesInProductDetails()
+        {
+            return View();
+        }
+    }
+}
---
+++ TechReviewPoint/Controllers/ReviewController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TechReviewPoint.Controllers
+{
+    public class ReviewController : Controller
+    {
+        // GET: Review
+        public ActionResult ReviewsInProductDetails()
+        {
+            return View();
+        }
+    }
+}
