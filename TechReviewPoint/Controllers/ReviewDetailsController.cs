3a2e718194f80f70b3a9792621aa119f2b7309e2
ayshagazi <ayshagazi2477@gmail.com>
2021-09-24 21:58:42 +0600

    Checked

--- TechReviewPoint/Controllers/ReviewDetailsController.cs
+++ TechReviewPoint/Controllers/ReviewDetailsController.cs
@@ -1,16 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using TechReviewPoint.Models;
 
 namespace TechReviewPoint.Controllers
 {
     public class ReviewDetailsController : Controller
     {
+        private tech_review_pointEntities db = new tech_review_pointEntities();
+
         // GET: ReviewDetails
         public ActionResult ReviewDetails()
         {
+            /*
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Review review = db.Reviews.Find(id);
+            if (review == null)
+            {
+                return HttpNotFound();
+            }
+            return View(review);
+            */
             return View();
         }
     }
