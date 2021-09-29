f3b6490321aad3be6c38b7d2de712bd14c1bfe46
ayshagazi <ayshagazi2477@gmail.com>
2021-09-29 14:35:16 +0600

    Product search done

--- TechReviewPoint/Controllers/ProductDetailsController.cs
+++ TechReviewPoint/Controllers/ProductDetailsController.cs
@@ -44,15 +44,60 @@ namespace TechReviewPoint.Controllers
             }
             return View(pro);
         }
-
+        [HttpGet]
         public ActionResult search()
         {
-            return View();
+            using (db)
+            {
+               // var ads = db.Products.SqlQuery("Select *from Products where PriorityStatus>-1")
+
+                var ads = db.Products.SqlQuery("Select *from Products")
+                      .ToList<Product>();
+
+                //var ads = db.Products.SqlQuery("Select Categories.CategoryName,Products.ProductName,Products.ProductPrice from Products inner join Categories on Products.CategoryID=Categories.CategoryID").ToList<Product>();
+                
+               
+
+                ViewData["Ads"] = ads;
+                return View();
+            }
+           // return View();
         }
 
-        
-    
-        
-    }
+        [HttpPost]
+        public ActionResult search(TempSearch ta)
+        {
+            if (ModelState.IsValid)
+            {
+
+               // var ads = db.Products.SqlQuery("Select Categories.CategoryName,Products.ProductName,Products.ProductPrice from Products inner join Categories on Products.CategoryID=Categories.CategoryID where CategoryName like '%" + (ta.CategoryName ?? "%") + "%'").ToList<Product>();
+
+
+               // string t = "Select Categories.CategoryName,Products.ProductName,Products.ProductPrice from Products inner join Categories on Products.CategoryID=Categories.CategoryID where CategoryName like '%" + (ta.CategoryName ?? "%") + "%'";
+
+
+                var ads = db.Products.SqlQuery("Select *from Products where ProductName like '%" + (ta.ProductName ?? "%") + "%'")
+                   .ToList<Product>();
+
+                 string t = "Select *from Products where ProductName like '%" + (ta.ProductName ?? "%") + "%'";
+                System.Diagnostics.Debug.WriteLine(t);
+                if (ads == null)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ViewData["Ads"] = ads;
+                    return View();
+                }
+
+            }
+
+            return View();
+
+        }
+
+
+        }
 
 }
---
+++ TechReviewPoint/Models/TempSearch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TechReviewPoint.Models
+{
+    public class TempSearch
+    {
+        [DataType(DataType.Text)]
+      [Display(Name ="Product Name")]
+        public string ProductName { get; set; }
+        // [Required]
+       // [DataType(DataType.Text)]
+       // [Display(Name = "Selling Product")]
+       [DataType(DataType.Text)]
+       [Display(Name ="Category Name")]
+        public string CategoryName { get; set; }
+
+        [Display(Name ="Product Price")]
+        public decimal ProductPrice { get; set; }
+
+    }
+}
